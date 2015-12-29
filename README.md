1a3f74a44e11fa0446face474ba349d8298e9a9a
Ryan Thompson <service@aiwebsystems.com>
2015-12-29 16:32:36 -0600

    K

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS SDK
+
+Easily integrate Amazon Web Services into your Streams Platform projects.
---
+++ resources/lang/en/addon.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'        => 'Amazon SDK',
+    'description' => 'Easily integrate Amazon Web Services into your Streams Platform projects.'
+];
